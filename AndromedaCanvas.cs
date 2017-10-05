ae8b42c144d87b094e42fcfc6cbeb333953da4bd
Jonathan Holmes <Vorlias@users.noreply.github.com>
2017-10-05 13:26:43 +1300

    Implement AndromedaCanvasApplication

--- AndromedaCanvas.cs
+++ AndromedaCanvas.cs
@@ -1,13 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using Andromeda2D.System;
+using System;
+using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace Andromeda.Canvas
 {
     public class AndromedaCanvas : UserControl
     {
+        private Andromeda2D.System.Application _application;
+        internal AndromedaCanvasApplication _controller;
+
+        protected void InitializeComponent()
+        {
+
+        }
+
+        protected void Start()
+        {
+
+        }
+
+        public AndromedaCanvas()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (_controller == null)
+                base.OnPaint(e);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
+            if (_controller == null)
+                base.OnPaintBackground(pevent);
+        }
     }
 }
---
+++ AndromedaCanvasApplication.cs
@@ -0,0 +1,57 @@
+﻿using Andromeda.Canvas;
+using Andromeda2D.System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andromeda.Canvas
+{
+    public class AndromedaCanvasApplication : Application
+    {
+        private BackgroundWorker _renderWorker;
+        private AndromedaCanvas _control;
+        public delegate void CanvasFrameEvent();
+
+        public event CanvasFrameEvent OnUpdate;
+        public event CanvasFrameEvent OnRender;
+
+        internal AndromedaCanvasApplication(AndromedaCanvas handle) : base(handle.Handle)
+        {
+            _renderWorker = new BackgroundWorker();
+            handle._controller = this;
+            _control = handle;
+        }
+
+        protected override void Update()
+        {
+            OnUpdate?.Invoke();
+        }
+
+        protected override void Render()
+        {
+            OnRender?.Invoke();
+        }
+
+        public override void Run()
+        {
+            InitializeApplication();
+            _renderWorker.DoWork += WorkProcess;
+            _renderWorker.RunWorkerAsync(_control);
+        }
+
+        private void WorkProcess(object sender, DoWorkEventArgs e)
+        {
+            while (Window.IsOpen)
+            {
+                UpdateDeltaClock();
+
+                UpdateEvents();
+
+                UpdateRendering();
+            }
+        }
+    }
+}
